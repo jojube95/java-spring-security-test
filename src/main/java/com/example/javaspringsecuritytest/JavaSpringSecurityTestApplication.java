febbe2a1d95e9b6d5712af2b35bf77f7d28d0ef3
jojube95 <joanjube95@gmail.com>
2023-04-25 18:57:28 +0200

    feat: generate Spring Boot base project

---
+++ src/main/java/com/example/javaspringsecuritytest/JavaSpringSecurityTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringsecuritytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringSecurityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringSecurityTestApplication.class, args);
+	}
+
+}
